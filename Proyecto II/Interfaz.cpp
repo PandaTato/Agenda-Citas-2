bddb68859e97031daa627a241c90c5829f7b4252
PandaTato <44411859+PandaTato@users.noreply.github.com>
2018-10-26 18:19:15 -0700

    Interfaz 1.0

---
+++ Proyecto II/Interfaz.cpp
@@ -0,0 +1,199 @@
+#include "Interfaz.h"
+
+Empresa * Interfaz::inicio()
+{
+	return nullptr;
+}
+
+int Interfaz::menu()
+{
+	return 0;
+}
+
+int Interfaz::MFactura()
+{
+	return 0;
+}
+
+bool Interfaz::MFactura(Empresa *)
+{
+	return false;
+}
+
+int Interfaz::mantenimiento()
+{
+	return 0;
+}
+
+int Interfaz::MMFactura()
+{
+	return 0;
+}
+
+bool Interfaz::MMFIingresar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMFImodificar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMFIeliminar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMFImostrarid(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMFImostrartodos(Empresa *)
+{
+	return false;
+}
+
+int Interfaz::MMCliente()
+{
+	return 0;
+}
+
+bool Interfaz::MMCingresar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMCmodificar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMCeliminar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMCmostrarid(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMCmostrartodos(Empresa *)
+{
+	return false;
+}
+
+int Interfaz::MMDoctor()
+{
+	return 0;
+}
+
+bool Interfaz::MMDingresar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMDmodificar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMDeliminar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMDmostrarid(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMDmostrartodos(Empresa *)
+{
+	return false;
+}
+
+int Interfaz::MMCita()
+{
+	return 0;
+}
+
+bool Interfaz::MMCIingresar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMCImodificar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMCIeliminar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMCImostrarid(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMCImostrartodos(Empresa *)
+{
+	return false;
+}
+
+int Interfaz::MMViaje()
+{
+	return 0;
+}
+
+bool Interfaz::MMVingresar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMVmodificar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMVeliminar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMVmostrarid(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMVmostrartodos(Empresa *)
+{
+	return false;
+}
+
+int Interfaz::MMEmpresa()
+{
+	return 0;
+}
+
+bool Interfaz::MMEmodificar(Empresa *)
+{
+	return false;
+}
+
+bool Interfaz::MMEmostrar(Empresa *)
+{
+	return false;
+}
+
+void Interfaz::salir()
+{
+}
+
+void Interfaz::defaul()
+{
+}
---
+++ Proyecto II/Interfaz.h
@@ -0,0 +1,72 @@
+#ifndef INTERFAZ_H
+#define ITERFAZ_H
+
+class Empresa;
+
+class Interfaz
+{
+public:
+	static Empresa* inicio();
+
+	static int menu();
+
+	//venta
+	static int MFactura();
+	static bool MFactura(Empresa*);
+
+	//mantenimiento
+	static int mantenimiento();
+
+
+	static int MMFactura();
+	//Reservas mantenimiento
+	static bool MMFIingresar(Empresa*);
+	static bool MMFImodificar(Empresa*);
+	static bool MMFIeliminar(Empresa*);
+	static bool MMFImostrarid(Empresa*);
+	static bool MMFImostrartodos(Empresa*);
+
+	static int MMCliente();
+	//cliente mantenimiento
+	static bool MMCingresar(Empresa*);
+	static bool MMCmodificar(Empresa*);
+	static bool MMCeliminar(Empresa*);
+	static bool MMCmostrarid(Empresa*);
+	static bool MMCmostrartodos(Empresa*);
+
+	static int MMDoctor();
+	//Pilotos mantenimiento
+	static bool MMDingresar(Empresa*);
+	static bool MMDmodificar(Empresa*);
+	static bool MMDeliminar(Empresa*);
+	static bool MMDmostrarid(Empresa*);
+	static bool MMDmostrartodos(Empresa*);
+
+	static int MMCita();
+	//Aviones mantenimiento
+	static bool MMCIingresar(Empresa*);
+	static bool MMCImodificar(Empresa*);
+	static bool MMCIeliminar(Empresa*);
+	static bool MMCImostrarid(Empresa*);
+	static bool MMCImostrartodos(Empresa*);
+
+	static int MMViaje();
+	//Aviones mantenimiento
+	static bool MMVingresar(Empresa*);
+	static bool MMVmodificar(Empresa*);
+	static bool MMVeliminar(Empresa*);
+	static bool MMVmostrarid(Empresa*);
+	static bool MMVmostrartodos(Empresa*);
+
+	static int MMEmpresa();
+	//empresa mantenimiento
+	static bool MMEmodificar(Empresa*);
+	static bool MMEmostrar(Empresa*);
+
+
+	static void salir();
+	static void defaul();
+
+};
+
+#endif // !INTERFAZ_H
